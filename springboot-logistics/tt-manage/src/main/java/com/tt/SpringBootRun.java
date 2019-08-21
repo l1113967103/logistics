673c8625b8833285ca4db6bcba1031b95a7dca6a
dabaicai <1113967103@qq.com>
2019-08-21 11:03:21 +0800

    add springbootrun yml etc

---
+++ springboot-logistics/tt-manage/src/main/java/com/tt/SpringBootRun.java
@@ -0,0 +1,12 @@
+package com.tt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRun {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRun.class, args);
+	}
+}
